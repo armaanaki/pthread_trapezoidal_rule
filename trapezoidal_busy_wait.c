652a29dc8db4b090ee64bd3c17977d1b92c42541
Armaan Esfahani <armaanaki@gmail.com>
2019-09-25 12:04:54 -0400

    working code for all three versions
    Signed-off-by: Armaan Esfahani <armaanaki@gmail.com>

--- trapezoidal_busy_wait.c
+++ trapezoidal_busy_wait.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include "timer.h"
 
-long thread_count;
+long thread_count, per_thread;
 volatile int flag;
-volatile double sum;
+volatile double sum, h;
+volatile long a, b, n;
 
 void* trapezoidal_rule(void* rank);
 
 void usage(char* prog_name);
 void get_args(int argc, char* argv[]);
+double function(double x);
 
 
 int main(int argc, char* argv[]) {
@@ -19,15 +22,23 @@ int main(int argc, char* argv[]) {
 
     /* get args and set rng */
     get_args(argc, argv);
+    per_thread = n/thread_count;
 
     thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t));
 
+    h = (b-a)/(double) n;
+    sum = (function(a) + function(b))/2.0;
 
+    GET_TIME(start);
     for (thread = 0; thread < thread_count; thread++) 
         pthread_create(&thread_handles[thread], NULL, trapezoidal_rule, (void*) thread);
     for (thread = 0; thread < thread_count; thread++)
         pthread_join(thread_handles[thread], NULL);
 
+    sum = h * sum;
+    GET_TIME(finish);
+
+    printf("approximation is: %.15f\n", sum);
     printf("time = %e seconds\n", finish - start);
     free(thread_handles);
 }
@@ -42,13 +53,31 @@ int main(int argc, char* argv[]) {
 void* trapezoidal_rule(void* rank) {
     long my_rank = (long) rank;
     double local_sum;
+    double x_i;
+    long my_start, my_end;
 
-    /* CODE FOR TRAPEZOIDAL RULE GOES HERE*/
+    my_start = my_rank * per_thread;
+    my_end = my_start + per_thread;
+
+    for (my_start; my_start < my_end; my_start++) {
+        x_i = a + my_start*h;
+        local_sum += function(x_i);
+    }
 
     while (flag != my_rank);
     sum += local_sum;
     flag = (flag+1) % thread_count;
     return NULL;
+}
+
+/*
+ * Function: function
+ * Purpose: return an f(x) given x
+ * In args: x
+ * Out args: f(x) or y
+ */
+double function(double x) {
+    return x*x;
 }
 
 /*
@@ -58,9 +87,14 @@ void* trapezoidal_rule(void* rank) {
  * Globals out: thread_count
  */
 void get_args(int argc, char* argv[]) {
-    if (argc != 2) usage(argv[0]);
+    if (argc != 5) usage(argv[0]);
     thread_count = strtol(argv[1], NULL, 10);
     if (thread_count < 0) usage(argv[0]);
+    n = strtol(argv[2], NULL, 10);
+    if (n % thread_count != 0) usage(argv[0]);
+    a = strtol(argv[3], NULL, 10);
+    b = strtol(argv[4], NULL, 10);
+    if (a > b) usage(argv[0]);
 }
 
 /*
@@ -69,7 +103,9 @@ void get_args(int argc, char* argv[]) {
  * In args:     prog_name
  */
 void usage(char* prog_name) {
-    fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
-    fprintf(stderr, "number of threads \n");
+    fprintf(stderr, "usage: %s <number of threads> <n> <a> <b>\n", prog_name);
+    fprintf(stderr, "a is one side of integral, b is the other. n are the total subintervals\n");
+    fprintf(stderr, "number of threads should be divisble by n\n");
+    fprintf(stderr, "a < b\n");
     exit(1);
 }
--- trapezoidal_semaphore.c
+++ trapezoidal_semaphore.c
@@ -2,15 +2,18 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include "timer.h"
 
-long thread_count;
-volatile double sum;
+long thread_count, per_thread;
+volatile double sum, h;
+volatile long a, b, n;
 sem_t semaphore;
 
 void* trapezoidal_rule(void* rank);
 
 void usage(char* prog_name);
 void get_args(int argc, char* argv[]);
+double function(double x);
 
 
 int main(int argc, char* argv[]) {
@@ -20,17 +23,27 @@ int main(int argc, char* argv[]) {
 
     /* get args and set rng */
     get_args(argc, argv);
+    per_thread = n/thread_count;
 
     thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t));
+    sem_init(&semaphore, 0, 1);
 
+    h = (b-a)/(double) n;
+    sum = (function(a) + function(b))/2.0;
 
+    GET_TIME(start);
     for (thread = 0; thread < thread_count; thread++) 
         pthread_create(&thread_handles[thread], NULL, trapezoidal_rule, (void*) thread);
     for (thread = 0; thread < thread_count; thread++)
         pthread_join(thread_handles[thread], NULL);
 
+    sum = h * sum;
+    GET_TIME(finish);
+
+    printf("approximation is: %.15f\n", sum);
     printf("time = %e seconds\n", finish - start);
 
+    sem_destroy(&semaphore);
     free(thread_handles);
 }
 
@@ -42,14 +55,33 @@ int main(int argc, char* argv[]) {
  * Global out: 
  */
 void* trapezoidal_rule(void* rank) {
+    long my_rank = (long) rank;
     double local_sum;
+    double x_i;
+    long my_start, my_end;
 
-    /* CODE FOR TRAPEZOIDAL RULE GOES HERE*/
+    my_start = my_rank * per_thread;
+    my_end = my_start + per_thread;
+
+    for (my_start; my_start < my_end; my_start++) {
+        x_i = a + my_start*h;
+        local_sum += function(x_i);
+    }
 
     sem_wait(&semaphore);
     sum += local_sum;
     sem_post(&semaphore);
     return NULL;
+}
+
+/*
+ * Function: function
+ * Purpose: return an f(x) given x
+ * In args: x
+ * Out args: f(x) or y
+ */
+double function(double x) {
+    return x*x;
 }
 
 /*
@@ -59,9 +91,14 @@ void* trapezoidal_rule(void* rank) {
  * Globals out: thread_count
  */
 void get_args(int argc, char* argv[]) {
-    if (argc != 2) usage(argv[0]);
+    if (argc != 5) usage(argv[0]);
     thread_count = strtol(argv[1], NULL, 10);
     if (thread_count < 0) usage(argv[0]);
+    n = strtol(argv[2], NULL, 10);
+    if (n % thread_count != 0) usage(argv[0]);
+    a = strtol(argv[3], NULL, 10);
+    b = strtol(argv[4], NULL, 10);
+    if (a > b) usage(argv[0]);
 }
 
 /*
@@ -70,7 +107,9 @@ void get_args(int argc, char* argv[]) {
  * In args:     prog_name
  */
 void usage(char* prog_name) {
-    fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
-    fprintf(stderr, "number of threads \n");
+    fprintf(stderr, "usage: %s <number of threads> <n> <a> <b>\n", prog_name);
+    fprintf(stderr, "a is one side of integral, b is the other. n are the total subintervals\n");
+    fprintf(stderr, "number of threads should be divisble by n\n");
+    fprintf(stderr, "a < b\n");
     exit(1);
 }
