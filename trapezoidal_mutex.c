3223632669a771919ea540f647e926bc0f325e58
Armaan Esfahani <armaanaki@gmail.com>
2019-09-25 00:24:12 -0400

    templates with timer
    Signed-off-by: Armaan Esfahani <armaanaki@gmail.com>

---
+++ trapezoidal_mutex.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+long thread_count;
+pthread_mutex_t mutex;
+
+void* trapezoidal_rule;
+
+void usage(char* prog_name);
+void get_args(int argc, char* argv[]);
+
+
+int main(int argc, char* argv[]) {
+    long thread;
+    pthread_t* thread_handles;
+    double start, finish;
+
+    /* get args and set rng */
+    get_args(argc, argv);
+
+    thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t));
+    pthread_mutex_init(&mutex, NULL);
+
+
+    for (thread = 0; thread < thread_count; thread++) 
+        pthread_create(&thread_handles[thread], NULL, trapezoidal_rule, (void*) thread);
+    for (thread = 0; thread < thread_count; thread++)
+        pthread_join(thread_handles[thread], NULL);
+
+    printf("time = %e seconds\n", finish - start);
+
+    pthread_mutex_destroy(&mutex);
+    free(thread_handles);
+    return 0;
+}
+
+/*
+ * Function: trapezoidal_rule 
+ * Purpose: 
+ * In args: 
+ * Global out: 
+ */
+void* trapezoidal_rule(void* rank) {
+    pthread_mutex_lock(&mutex);
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+/*
+ * Function:    get_args
+ * Purpose:     get command line args
+ * In args:     argc, argv
+ * Globals out: thread_count
+ */
+void get_args(int argc, char* argv[]) {
+    if (argc != 2) usage(argv[0]);
+    thread_count = strtol(argv[1], NULL, 10);
+    if (thread_count < 0) usage(argv[0]);
+}
+
+/*
+ * Function:    usage
+ * Purpose:     print usage if program run incorrectly
+ * In args:     prog_name
+ */
+void usage(char* prog_name) {
+    fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
+    fprintf(stderr, "number of threads \n");
+    exit(1);
+}
---
+++ trapezoidal_semaphore.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+long thread_count;
+volatile int flag;
+
+void* trapezoidal_rule;
+
+void usage(char* prog_name);
+void get_args(int argc, char* argv[]);
+
+
+int main(int argc, char* argv[]) {
+    long thread;
+    pthread_t* thread_handles;
+    double start, finish;
+
+    /* get args and set rng */
+    get_args(argc, argv);
+
+    thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t));
+
+
+    for (thread = 0; thread < thread_count; thread++) 
+        pthread_create(&thread_handles[thread], NULL, trapezoidal_rule, (void*) thread);
+    for (thread = 0; thread < thread_count; thread++)
+        pthread_join(thread_handles[thread], NULL);
+
+    printf("time = %e seconds\n", finish - start);
+
+    free(thread_handles);
+}
+
+/*
+ * Function: trapezoidal_rule 
+ * Purpose: 
+ * In args: 
+ * Global in: thread_count
+ * Global out: 
+ */
+void* trapezoidal_rule(void* rank) {
+    long my_rank = (long) rank;
+    return NULL;
+}
+
+/*
+ * Function:    get_args
+ * Purpose:     get command line args
+ * In args:     argc, argv
+ * Globals out: thread_count
+ */
+void get_args(int argc, char* argv[]) {
+    if (argc != 2) usage(argv[0]);
+    thread_count = strtol(argv[1], NULL, 10);
+    if (thread_count < 0) usage(argv[0]);
+}
+
+/*
+ * Function:    usage
+ * Purpose:     print usage if program run incorrectly
+ * In args:     prog_name
+ */
+void usage(char* prog_name) {
+    fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
+    fprintf(stderr, "number of threads \n");
+    exit(1);
+}
